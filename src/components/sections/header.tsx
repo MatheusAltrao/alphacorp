5a4cc7ed1d8297fe1283c946391e749b600325b0
Matheus Altrao <matheusaltrao2@gmail.com>
2025-10-30 14:31:40 -0400

    style(header): Adiciona classes de estilo ao componente `Header` para fixá-lo no topo

--- src/components/sections/header.tsx
+++ src/components/sections/header.tsx
@@ -6,7 +6,7 @@ import MenuMobile from "./menu-mobile";
 
 export default function Header() {
   return (
-    <header>
+    <header className="fixed top-0 w-full z-50 bg-background">
       <nav className="bg-background sticky top-0 isolate z-50 border-b py-3.5 md:py-4">
         <div className="relative container m-auto flex flex-col justify-between gap-4 px-6 md:flex-row md:items-center md:gap-6">
           <div className="flex items-center justify-between">
