15222ca019b4c87e67296bcae00be6e497ca6ad7
Matheus Altrao <matheusaltrao2@gmail.com>
2025-12-21 15:25:22 -0400

    feat(const/links): Adiciona novo item 'Home' ao array `MENU_ITEMS`

--- src/const/links.ts
+++ src/const/links.ts
@@ -1,4 +1,5 @@
 export const MENU_ITEMS = [
-  { label: 'How it works', href: '#how-it-works' },
-  { label: 'Metrics', href: '#results' },
-] as const
+  { label: "Home", href: "#home" },
+  { label: "How it works", href: "#how-it-works" },
+  { label: "Metrics", href: "#results" },
+] as const;
